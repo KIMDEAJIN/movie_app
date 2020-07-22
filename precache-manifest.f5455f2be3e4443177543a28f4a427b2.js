ff2537a795e1dce4fa28d5a8b07cdfbda701c08c
Aas kim <djssramg94@naver.com>
2020-07-22 23:42:16 +0900

    Updates

---
+++ precache-manifest.f5455f2be3e4443177543a28f4a427b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec13471c300280d74ee47ff01cb1c49f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "aadebc0f71c6a2a62fa5",
+    "url": "/movie_app/static/css/main.b1d0d8a0.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aadebc0f71c6a2a62fa5",
+    "url": "/movie_app/static/js/main.33206edd.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
